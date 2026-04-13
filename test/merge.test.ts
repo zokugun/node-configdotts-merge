e95cde1c5069db6ceade3246d45bc210e9bc45cb
Baptiste Augrain <daiyam@zokugun.org>
2026-04-13 17:46:55 +0200

    refactor: use vitest instead of mocha

--- test/merge.test.ts
+++ test/merge.test.ts
@@ -1,40 +1,47 @@
-import fs from 'fs';
-import path from 'path';
-import chai, { expect } from 'chai';
-import chaiAsPromised from 'chai-as-promised';
-import klaw from 'klaw-sync';
+import path from 'node:path';
+import fse from '@zokugun/fs-extra-plus/async';
+import { expect, it } from 'vitest';
 import YAML from 'yaml';
 import { merge } from '../src/index.js';
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-chai.use(chaiAsPromised);
+function prepare(file: string) {
+	const name = path.basename(file).slice(0, path.basename(file).lastIndexOf('.'));
 
-describe('merge', async () => {
-	function prepare(file: string) {
-		const name = path.basename(file).slice(0, path.basename(file).lastIndexOf('.'));
+	it(name, async () => {
+		const filePath = path.join(path.dirname(file), `${name}.yml`);
+		const readResult = await fse.readFile(filePath, 'utf8');
+		expect(readResult.fails).to.be.false;
 
-		it(name, () => {
-			const { file1, file2, output } = YAML.parse(fs.readFileSync(path.join(path.dirname(file), `${name}.yml`), 'utf8')) as { file1: string; file2: string; output: string };
-			const result = merge(file1, file2);
+		const { file1, file2, output } = YAML.parse(readResult.value!) as { file1: string; file2: string; output: string };
+		const result = merge(file1, file2);
 
-			try {
-				expect(output).to.eql(result);
-			}
-			catch (error: unknown) {
-				console.log(result);
+		try {
+			expect(output).to.eql(result);
+		}
+		catch (error: unknown) {
+			console.log(result);
 
-				throw error;
-			}
-		});
-	}
-
-	const files = klaw(path.join(__dirname, '..', '..', 'test', 'fixtures'), {
-		nodir: true,
-		traverseAll: true,
-		filter: (item) => item.path.endsWith('.yml'),
+			throw error;
+		}
 	});
+}
 
-	for(const file of files) {
-		prepare(file.path);
-	}
+const walkResult = await fse.walk(path.join(__dirname, 'fixtures'), {
+	absolute: true,
+	onlyFiles: true,
+	filter: (item) => item.path.endsWith('.yml'),
 });
+
+if(walkResult.fails) {
+	throw walkResult.error;
+}
+else {
+	for await (const file of walkResult.value) {
+		if(file.fails) {
+			throw file.error;
+		}
+		else {
+			prepare(file.value.path);
+		}
+	}
+}
